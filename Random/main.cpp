2163f1697802291a464ee274c963498ada54ee2c
Sidor_md45@top-academy.ru <Sidor_md45@top-academy.ru>
2024-04-02 17:03:52 +0300

    endcoding

---
+++ Random/main.cpp
@@ -0,0 +1,61 @@
+﻿#include<iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+void main() {
+	setlocale(LC_ALL, "");
+	const int SIZE = 5;
+	int arr[SIZE];
+	int minRand, maxRand;
+	cout << "Введите минимальное случайное число: "; cin >> minRand;
+	cout << "Ввдеите максимальное случайное число: "; cin >> maxRand;
+	
+	if (maxRand == minRand) {
+		cout << "Error 0: minRand != maxRand";
+		return;
+	}
+	else if (minRand > maxRand) {
+		cout << "Error 1: vaule minRand can`t be batter than maxRand";
+		return;
+	}
+
+	//Запонения массива случайными числами: 
+
+	for (int i = 0; i < SIZE; i++) {
+		arr[i] = rand() % (maxRand  - minRand) + minRand;
+	}
+
+	//Вывод массива на экран: 
+	for (int i = 0; i < SIZE; i++) 
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+	cout << endl;
+	//Сортировка массива: 
+	
+	for (int i = 0; i < SIZE; i++) {
+	
+		for (int j = i + 1; j < SIZE; j++) {
+			if (arr[j] < arr[i]) 
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+
+	}
+
+	//Вывод отсортированного массива: 
+	for (int i = 0; i < SIZE; i++) 
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+}
---
+++ Unique/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+#define tab "\t"
+
+void main() 
+{
+	setlocale(LC_ALL, "");
+	int const SIZE = 10;
+	int arr[SIZE];
+	for (int i = 0; i < SIZE; i++) 
+	{
+		int a = arr[i] = rand() % (30 - 20) + 20;
+	
+	
+	}
+
+
+	//for (int i = 0; i< SIZE; i++) 
+	//{
+	//	cout << arr[i] << tab;
+	//}
+}
